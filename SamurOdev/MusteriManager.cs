7ca2ff46c9574ae23097e67e634672a80991dece
mustafa.smrr@gmail.com <mustafa.smrr@gmail.com>
2021-01-27 14:36:07 +0300

    Proje dosyası ekle.

---
+++ SamurOdev/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamurOdev
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri m)
+        {
+            Console.WriteLine($"{m.Adi} {m.Soyadi} isimli müşteri eklendi!");
+        }
+
+        public void Sil()
+        {
+            Console.WriteLine("Silme İşlemi Tamamlandı");
+        }
+        public void Listele()
+        {
+            Console.WriteLine("Müşteri listesi listelendi");
+        }
+
+        public void Guncelle(Musteri m)
+        {
+            Console.WriteLine($"{m.Adi} {m.Soyadi} müşterisi güncellendi");
+        }
+    }
+}
---
+++ SamurOdev/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamurOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m = new Musteri { Adi = "Hulusi", Soyadi = "DEMİR", Id = 1 };
+            Musteri m2 = new Musteri { Adi = "Mustafa", Soyadi = "SAMUR", Id = 2 };
+            Musteri m3 = new Musteri { Adi = "Fatih", Soyadi = "GÜMÜŞ", Id = 3 };
+
+            MusteriManager manager = new MusteriManager();
+            manager.Ekle(m);
+            manager.Ekle(m2);
+            manager.Ekle(m3);
+            manager.Listele();
+            manager.Guncelle(m2);
+
+            Console.ReadKey();
+        }
+    }
+}
